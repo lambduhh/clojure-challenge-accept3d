3adb6912b23554c0b4e4b3c447b7b4ba138c7119
lorilynjmiller <lorilynjmiller@gmail.com>
2020-01-09 18:50:54 -0500

    -fizzbuzz comments

--- src/clojure_challenge_accept3d/fizzbuzz.clj
+++ src/clojure_challenge_accept3d/fizzbuzz.clj
@@ -1,6 +1,6 @@
 (ns clojure-challenge-accept3d.fizzbuzz)
 
-
+;; first implementation of fizzbuzz
 (defn div-by-5-and-3 [n]
   (if (and (zero? (rem n 3)) (zero? (rem n 5)))
     true))
@@ -13,8 +13,10 @@
       (zero? (rem i 5)) (println "Buzz")
       :else (println i)
       )))
+;; ran code in HR but failed 2 test cases based on code "running too slow"?
 
 
+;; second implementation to fizzbuzz to test if I would receive the same failures (test cases 6 and 7)
 (defn fizzbuzz1 [n]
   (doseq [i (range 1 (inc n))]
     (case [(zero? (rem i 3)) (zero? (rem i 5))]
@@ -22,6 +24,7 @@
       [true false] (println "Fizz")
       [false true] (println "Buzz")
       [true true] (println "FizzBuzz"))))
+;; same failures on cases 6 and 7
 
 (comment
   (fizzbuzz 15)
