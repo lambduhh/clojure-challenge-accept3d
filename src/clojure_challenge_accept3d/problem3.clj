33f8cfab731a883631eed3ca4ebd342fee8870d2
lorilynjmiller <lorilynjmiller@gmail.com>
2020-01-09 19:54:21 -0500

    -math for problem 3

--- src/clojure_challenge_accept3d/problem3.clj
+++ src/clojure_challenge_accept3d/problem3.clj
@@ -1,3 +1,15 @@
 (ns clojure-challenge-accept3d.problem3)
 
+(defn factorial [n]
+  (if (zero? n)
+    1
+    (* n (factorial (dec n)))))
+
+(defn nperms [n r]
+  (/ (factorial n) (factorial (- n r))))
+
+(comment
+  (nperms 10 4)
+  )
+
 
