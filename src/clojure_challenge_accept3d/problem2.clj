9ebe4ce07ed8364347af8ddb780d15105914fc9b
lorilynjmiller <lorilynjmiller@gmail.com>
2020-01-09 19:13:26 -0500

    -problem2

--- src/clojure_challenge_accept3d/problem2.clj
+++ src/clojure_challenge_accept3d/problem2.clj
@@ -1,7 +1,21 @@
 (ns clojure-challenge-accept3d.problem2)
 
+(declare maxDifference)
+
+
+(defn diffnums [n xs]
+  (if (empty? xs) nil
+                  (max (maxDifference (into [n] xs))
+                       (diffnums n (rest xs)))))
+
 (defn maxDifference [nums]
-  (- (max nums) (min nums)))
+  (cond
+    (= 1 (count nums)) -1
+    (= 2 (count nums)) (if (< (first nums) (second nums))
+                         (- (second nums) (first nums)))
+    :else
+    (max (diffnums (first nums) (rest nums))
+         (maxDifference (rest nums)))))
 
 
 
