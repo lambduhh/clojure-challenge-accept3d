a992298a094a81d1211f8c7b1c90b7a7199c2560
lorilynjmiller <lorilynjmiller@gmail.com>
2020-01-09 20:08:05 -0500

    -tools for problem3

--- src/clojure_challenge_accept3d/problem3.clj
+++ src/clojure_challenge_accept3d/problem3.clj
@@ -8,8 +8,45 @@
 (defn nperms [n r]
   (/ (factorial n) (factorial (- n r))))
 
+
 (comment
   (nperms 10 4)
+
+  (nperms 4 4)
   )
+
+
+
+
+(defn num->nums [num-string]
+  (str (Integer/parseInt num-string)))
+
+(comment
+
+  (num->nums "1")
+  (num->nums "0110")
+  (num->nums "1100"))
+
+(defn counter [s]
+  (reduce (fn [res x] (update res x (fnil inc 0))) {} s))
+
+(comment
+  (counter "110")
+  (counter "1110"))
+
+(defn similar? [a b]
+  (and (= a (num->nums a))
+       (= b (num->nums b))
+       (= (counter (num->nums a))
+          (counter (num->nums b)))))
+
+(comment
+  (similar? "110" "1110")
+  (similar? "0110" "110")
+  (similar? "1100" "0110")
+
+  
+  )
+
 
 
