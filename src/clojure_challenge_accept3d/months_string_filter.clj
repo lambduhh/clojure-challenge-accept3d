419821b25df0fe7e9201f9428c2cf5e4b7cdba20
lorilynjmiller <lorilynjmiller@gmail.com>
2020-01-14 18:20:10 -0500

    -months-string-filter refactor

--- src/clojure_challenge_accept3d/months_string_filter.clj
+++ src/clojure_challenge_accept3d/months_string_filter.clj
@@ -14,54 +14,53 @@
 
 
 
-(def m-letters (set (vec "jfmamjjasond")))
+(def l-months (set (vec "jfmamjjasond")))
 
 (defn letter-data [[a b]]
-  {:first-letter a
-   :first-let-in-m? (m-letters a)
-   :second-letter b
-   :second-letter-in-m? (m-letters b)})
+  {:first-letter             a
+   :first-letter-in-months?  (get l-months a)
+   :second-letter            b
+   :second-letter-in-months? (get l-months b)})
 
-(defn return-lets-in-m [m]
-  (let [first-letter (:first-let-in-m? m)
-        second-letter (:second-letter-in-m? m)]
+(defn data->letter [data]
+  (let [{:keys [first-letter first-letter-in-months? second-letter second-letter-in-months?]} data]
     (cond
-      (and first-letter second-letter) [first-letter second-letter]
-      first-letter [first-letter]
-      second-letter [second-letter]
-      :else [] )))
+      (and first-letter-in-months? second-letter-in-months?) [first-letter second-letter]
+      first-letter-in-months? [first-letter]
+      second-letter-in-months? [second-letter]
+      :else [])))
 
 (comment
-  (letter-data [a b])                                       ;;-> {:first-letter a, :first-let-in-m? False, ...}
-  (return-lets-in-m  {:first-letter \o, :first-let-in-m? \o, :second-letter \g, :second-letter-in-m? nil}) ;-> [\o]
-  (return-lets-in-m {:first-letter \g, :first-let-in-m? nil, :second-letter nil, :second-letter-in-m? nil})
-    )
-
-
-(defn month-string-filter [s]
-  (->> (partition-all 2 1 s)
-       (map letter-data)
-       (map return-lets-in-m)
-       (map (partial apply str))
-       (apply str)
-       ))
-
-
-(comment
-  (def s "cog")
-  (def ss (partition-all 2 1 s))
-  (def sss [{:first-letter \c, :first-let-in-m? nil, :second-letter \o, :second-letter-in-m? \o}
-            {:first-letter \o, :first-let-in-m? \o, :second-letter \g, :second-letter-in-m? nil}
-            {:first-letter \g, :first-let-in-m? nil, :second-letter nil, :second-letter-in-m? nil}])
-  (def ssss (map return-lets-in-m sss))
-  (def sssss (map (partial apply str) ssss))
-  (apply str sssss)
-
-
-  (month-string-filter "cognitect")
-
-
-
+  (data->letter {:first-letter \o, :first-letter-in-months? \o, :second-letter \g, :second-letter-in-months? \g})
+  ;;=>[\o \g]
+  (data->letter {:first-letter \o, :first-letter-in-months? \o, :second-letter \g, :second-letter-in-months? nil})
+  ;;=>[\o]
+  (data->letter {:first-letter \o, :first-letter-in-months? nil, :second-letter \g, :second-letter-in-months? \g})
+  ;;=>[\o]
+  (data->letter {:first-letter \o, :first-letter-in-months? nil, :second-letter \g, :second-letter-in-months? nil})
+  ;;=> []
   )
 
+(defn vec->str [v]
+  (apply str v))
 
+(defn months-string-filter [s]
+  (->> (partition-all 2 1 s)
+       (map letter-data)
+       (map data->letter)
+       (mapv vec->str)
+       (vec->str)))
+
+
+(comment
+  (def s1 [[\c \o] [\o \g] [\g]])
+  (def s2 (map letter-data s1))
+  (map data->letter s2)
+  ;;-> [[\o][\o]]
+  (def s3 [[\o] [\o] []])
+  (def s4 (mapv vec->str s3))
+  ;=> ("o" "o")
+  (vec->str s4)
+  (months-string-filter "cog")
+  ;-> "oo"
+  )
