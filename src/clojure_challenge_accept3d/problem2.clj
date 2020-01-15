e823c2811a5d0ca9fb59ab49b2fb29c41ab20635
lorilynjmiller <lorilynjmiller@gmail.com>
2020-01-15 11:17:05 -0500

    -OG with directions

--- src/clojure_challenge_accept3d/problem2.clj
+++ src/clojure_challenge_accept3d/problem2.clj
@@ -1,5 +1,26 @@
 (ns clojure-challenge-accept3d.problem2)
-
+;Give an array of integers, compute the maximum difference between any item and any
+; lower indexed smaller item for all possible pairs. In other words, for the array arr,
+; find the maximum value of arr[j] - arr[i] for all i, j where 0 <= i < j < n and arr[i] < arr[j].
+; If no item has a smaller item with a lower index, then return -1.
+;
+;For example, given arr = [1,2,6,4],
+; first compare 2 to the elements to its left.
+; 1 is smaller, so calculate the difference 2 - 1 = 1.
+; 6 is bigger than 2 and 1, so calculate the difference 6 - 2 = 4 and 6 - 1 = 5.
+; The last element, 4, is only bigger than 2 and 1, and the difference are 4 - 2 = 2 and 4 - 1 = 3.
+; The largest difference is 6 - 1 = 5.
+;
+;Function Description
+;
+;Complete the function maxDifference. The function must return an integer that represents the maximum difference in arr.
+;
+;maxDifference has the following parameter(s): arr[arr[0], arr[1],...arr[n-1]]: an array of integers.
+;
+;Constraints
+;
+;1 <= n <= 2*10^5
+;-10^6 <= arr[i] <= 10^6 where [0, n-1]
 
 
 (defn maxdifference [nums]
